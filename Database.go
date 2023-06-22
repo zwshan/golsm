0df4f2b3ce1847104b53f290ef898ea7a33f2d66
root <root@localhost.localdomain>
2023-06-22 13:44:39 +0800

    new  commit

---
+++ Database.go
@@ -0,0 +1,19 @@
+package lsm
+
+import (
+	"github.com/zwshan/golsm/sortTree"
+	"github.com/zwshan/golsm/ssTable"
+	"github.com/zwshan/golsm/wal"
+)
+
+type Database struct {
+	// 内存表
+	MemoryTree *sortTree.Tree
+	// SSTable 列表
+	TableTree *ssTable.TableTree
+	// WalF 文件句柄
+	Wal *wal.Wal
+}
+
+// 数据库，全局唯一实例
+var database *Database
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zwshan/golsm
+
+go 1.18
