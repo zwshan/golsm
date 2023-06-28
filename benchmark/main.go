36e668873867e8e3692eca82890f9c408f9a86d3
zwshan <zw.shan@outlook.com>
2023-06-28 19:43:55 +0800

    新增可用的test benchamark

--- benchmark/main.go
+++ benchmark/main.go
@@ -7,13 +7,13 @@ import (
 	"github.com/zwshan/golsm/Systemconfig"
 	"os"
 	"time"
+	"math"	
+
 )
 
 type TestValue struct {
 	A int64
-	B int64
-	C int64
-	D string
+	B string
 }
 
 func main() {
@@ -32,12 +32,13 @@ func main() {
 		Threshold:     3000,
 		CheckInterval: 3,
 	})
-	// query()
-	insert()
+
+	insertKVCouples(10)
+	queryinsertKVCouples()
 
 }
 
-func query() {
+func queryinsertKVCouples() {
 	start := time.Now()
 	v, _ := lsm.Get[TestValue]("aaaaaa")
 	elapse := time.Since(start)
@@ -50,40 +51,53 @@ func query() {
 	fmt.Println("查找 aazzzz 完成，消耗时间：", elapse)
 	fmt.Println(v)
 }
-func insert() {
 
+// func queryInsertKVCouples(key string) {
+// 	start := time.Now()
+// 	v, _ := lsm.Get[TestValue](key)
+// 	elapse := time.Since(start)
+// 	fmt.Println("查找", key, "完成，消耗时间：", elapse)
+// 	fmt.Println(v)
+// }
+
+
+
+func insertKVCouples(numKeys int) {
 	// 64 个字节
 	testV := TestValue{
 		A: 1,
-		B: 1,
-		C: 3,
-		D: "00000000000000000000000000000000000000",
+		B: "to test the insert and query, successful!",
 	}
 	
 	count := 0
 	start := time.Now()
 	key := []byte{'a', 'a', 'a', 'a', 'a', 'a'}
 	lsm.Set(string(key), testV)
-	for a := 0; a < 26; a++ {
-		for b := 0; b < 26; b++ {
-			for c := 0; c < 26; c++ {
-				for d := 0; d < 26; d++ {
-					for e := 0; e < 26; e++ {
-						for f := 0; f < 26; f++ {
-							key[0] = 'a' + byte(a)
-							key[1] = 'a' + byte(b)
-							key[2] = 'a' + byte(c)
-							key[3] = 'a' + byte(d)
-							key[4] = 'a' + byte(e)
-							key[5] = 'a' + byte(f)
-							lsm.Set(string(key), testV)
-							count++
-						}
-					}
-				}
-			}
-		}
+
+	// 计算需要的循环次数
+	totalLoops := int(math.Pow(26, float64(len(key))))
+
+	// 减少循环的数量
+	if numKeys > totalLoops {
+		numKeys = totalLoops
 	}
+
+	for i := 0; i < numKeys; i++ {
+		// 将 i 转换为对应的 key
+		index := i
+		for j := len(key) - 1; j >= 0; j-- {
+			key[j] = 'a' + byte(index%26)
+			index /= 26
+		}
+
+		lsm.Set(string(key), testV)
+		count++
+	}
+
 	elapse := time.Since(start)
-	fmt.Println("插入完成，数据量：", count, ",消耗时间：", elapse)
+	fmt.Println("插入完成，数据量：", count, "，消耗时间：", elapse)
 }
+
+
+
+
